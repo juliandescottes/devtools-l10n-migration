acbc56c2f1d602f335cda27c819ebd0de8257f0c
Julian Descottes <julian.descottes@gmail.com>
2016-09-08 18:56:13 +0200

    Support running several time the same migration conf

--- migrate/main.py
+++ migrate/main.py
@@ -22,10 +22,41 @@ def get_translation_from_dtd(dtd_path, entity_name):
 
 # Create a new properties file at the provided path.
 def create_properties_file(path):
-    print 'creating new *.properties file: %s' % path
+    print '[INFO] creating new *.properties file: %s' % path
     prop_file = open(path, 'w+')
     prop_file.write(LICENSE)
     prop_file.close()
+
+
+# Migrate a single string entry for a dtd to a properties file.
+def migrate_string(dtd_path, prop_path, dtd_name, prop_name):
+    if not os.path.isfile(dtd_path):
+        print '[ERROR] dtd path is invalid: %s' % dtd_path
+        return
+    translation = get_translation_from_dtd(dtd_path, dtd_name)
+
+    if not os.path.isfile(prop_path):
+        create_properties_file(prop_path)
+
+    new_line = prop_name + '=' + translation + '\n'
+
+    # Skip the string if it already exists in the destination file.
+    prop_as_str = open(prop_path, 'r').read()
+    if new_line in prop_as_str:
+        print '[WARNING] property already migrated, skipping: %s' % prop_name
+        return
+
+    # Skip the string and log an error if an existing entry is found, but with
+    # a different value
+    if ('\n' + prop_name + '=') in prop_as_str:
+        print '[ERROR] existing entry found, skipping: %s' % prop_name
+        return
+
+    print '[INFO] migrating %s in %s' % (prop_name, os.path.basename(prop_path))
+    with open(prop_path, 'a') as prop_file:
+        localization_notes = '\n# LOCALIZATION NOTE (%s)\n' % prop_name
+        prop_file.write(localization_notes)
+        prop_file.write(new_line)
 
 
 # Apply the migration instructions in the provided configuration file
@@ -44,22 +75,13 @@ def migrate_conf(conf_path, l10n_path):
 
         # Expected syntax : ${dtd_path}:${dtd_name} = ${prop_path}:${prop_name}
         before, after = line.split(' = ')
-        dtd_file, dtd_name = before.split(':')
-        prop_file, prop_name = after.split(':')
+        dtd_path, dtd_name = before.split(':')
+        prop_path, prop_name = after.split(':')
 
-        dtd_path = os.path.join(l10n_path, dtd_file)
-        if not os.path.isfile(dtd_path):
-            print 'dtd path: [%s] is invalid' % dtd_path
-            continue
-        translation = get_translation_from_dtd(dtd_path, dtd_name)
+        dtd_path = os.path.join(l10n_path, dtd_path)
+        prop_path = os.path.join(l10n_path, prop_path)
 
-        prop_path = os.path.join(l10n_path, prop_file)
-        if not os.path.isfile(prop_path):
-            create_properties_file(prop_path)
-
-        with open(prop_path, 'a') as prop_file:
-            prop_file.write('\n# LOCALIZATION NOTE (' + prop_name + ')\n')
-            prop_file.write(prop_name + '=' + translation + '\n')
+        migrate_string(dtd_path, prop_path, dtd_name, prop_name)
 
 
 def main():
@@ -74,9 +96,9 @@ def main():
     # Retrieve path to devtools localization files in l10n repository.
     devtools_l10n_path = os.path.join(args.path, 'devtools/client/')
     if not os.path.exists(devtools_l10n_path):
-        print 'l10n path: [%s] is invalid' % devtools_l10n_path
+        print '[ERROR] l10n path is invalid: %s' % devtools_l10n_path
         exit()
-    print 'l10n path: [%s] is valid' % devtools_l10n_path
+    print '[INFO] l10n path is valid: %s' % devtools_l10n_path
 
     # Retrieve configuration files to apply.
     if os.path.isdir(args.config):
@@ -84,12 +106,12 @@ def main():
     elif os.path.isfile(args.config):
         conf_files = [args.config]
     else:
-        print 'config path: [%s] is invalid' % args.config
+        print '[ERROR] config path is invalid: %s' % args.config
         exit()
 
     # Perform migration for each configuration file.
     for conf_file in conf_files:
-        print 'performing migration for [%s]' % conf_file
+        print '[INFO] performing migration for [%s]' % conf_file
         migrate_conf(conf_file, devtools_l10n_path)
 
 
